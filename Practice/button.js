36df123da3c22427df5847651909dadf3ede04e5
Paaras Bhandari <paaras@Paarass-MacBook-Pro.local>
2019-08-12 21:18:47 -0400

    auto

--- Practice/button.js
+++
@@ -1,3 +0,0 @@
-var button = document.getElementById("myButton");
-document.body.color = "red";
-var h = document.querySelector("h2").innerHTML("Hello");
--- herokudeploy.sh
+++
@@ -1,6 +0,0 @@
-git init
-heroku create $1 --buildpack mars/create-react-app
-git add .
-git commit -m "First commit - deploying"
-git push heroku master
-heroku open
